55f7a67060b42b95512cb60247f28e2efbf6d222
diegolb55 <diegolugobendezu@gmail.com>
2023-02-28 07:23:34 -0400

    added search and join components enter and exit

--- components/JoinEvent.js
+++ components/JoinEvent.js
@@ -1,17 +1,16 @@
-import { motion, AnimatePresence } from "framer-motion"
-import { RxCross1 } from "react-icons/rx"
 import styles from "@/styles/JoinEvent.module.css"
+import { motion } from "framer-motion"
+import { IoIosArrowBack } from "react-icons/io"
 
-export default function JoinEvent({ isOpen, close }) {
-
-
+export default function JoinEvent({ isOpen, toggle}){
     return (
         <motion.div className={styles.joinpopup}
-            animate={isOpen ? {left: 0} : {left: "100%"}}
+            animate={isOpen ? {left: 0} : {left:"100%"}}
         >
+
             <div className={styles.headingbar}>
-                <h4>Other Events</h4>
-                <RxCross1 onClick={() => close(false)}/>
+                <IoIosArrowBack/>
+                <h4 onClick={() => toggle(false)}>Other Events</h4>
             </div>
         </motion.div>
     )
